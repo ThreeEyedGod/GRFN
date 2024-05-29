f02e5c847bdeca34c5652ed143c8f6f1f6e611ea
Mande Burung <mandeburung@Mandes-MacBook-Air.local>
2024-05-29 11:23:17 +0530

    cleanup on cabal

--- dist-newstyle/build/aarch64-osx/ghc-9.4.7/grfn-1.0.0.0/build/autogen/PackageInfo_grfn.hs
+++ dist-newstyle/build/aarch64-osx/ghc-9.4.7/grfn-1.0.0.0/build/autogen/PackageInfo_grfn.hs
@@ -18,7 +18,7 @@ version :: Version
 version = Version [1,0,0,0] []
 
 synopsis :: String
-synopsis = ""
+synopsis = "Uniformly-random pre-factored number generation as per Kalai's algorithm"
 copyright :: String
 copyright = "2024 ThreeEyedGod"
 homepage :: String
