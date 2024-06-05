b1c2357c5d4f1286824bc51ee93eb819b1c1ff31
Mande Burung <mandeburung@Mandes-MacBook-Air.local>
2024-06-05 11:11:15 +0530

    cleanup, benchmark cleanup, ghc compiler options for parallel added

--- Bench/BenchFactoredRandomNumbers.hs
+++ Bench/BenchFactoredRandomNumbers.hs
@@ -4,14 +4,26 @@ import Test.Tasty.Bench
 main :: IO ()
 main =
   defaultMain
-    [ bgroup
-        "PreFactored Uniform Random Numbers"
+    [ 
+      -- bgroup
+      --   "PreFactored Uniform Random Numbers -Max"
+      --   [ 
+      --     bench "2^62-parallel " $ whnfIO (preFactoredNumOfBitSizePar 62),
+      --     bench "2^62-Non-Parallel " $ whnfIO (preFactoredNumOfBitSize 62),
+      --     bench "Regular-500 " $ whnfIO (genARandomPreFactoredNumberLTEn 500)
+      --   ]
+      -- bgroup
+      --   "PreFactored Uniform Random Numbers -Median"
+      --   [ bench "2^62-parallel " $ whnfIO (preFactoredNumOfBitSizePar 32),
+      --     bench "2^62-Non-Parallel " $ whnfIO (preFactoredNumOfBitSize 22),
+      --     bench "Regular-500 " $ whnfIO (genARandomPreFactoredNumberLTEn 500)
+      --   ]
+      bgroup
+        "PreFactored Uniform Random Numbers -Quick"
         [ 
-          -- bench "2^62-parallel " $ whnfIO (preFactoredNumOfBitSizePar 62),
-          -- bench "2^62-Non-Parallel " $ whnfIO (preFactoredNumOfBitSize 62),
-          -- bench "Regular-500 " $ whnfIO (genARandomPreFactoredNumberLTEn 500)
           bench "2^6-parallel " $ whnfIO (preFactoredNumOfBitSizePar 6),
           bench "2^6-Non-Parallel " $ whnfIO (preFactoredNumOfBitSize 6),
           bench "Regular-50 " $ whnfIO (genARandomPreFactoredNumberLTEn 50)
         ]
+
     ]
